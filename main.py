988ff5096f43c336c43cd85fc4ab1823358dd4e0
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-05 15:21:46 +0300

    Initial

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-Test
-Test
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+
+from config.config import TOKEN
+
+BOT_TOKEN = TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command(commands='start'))
+async def process_start_cmd(message: Message):
+    await message.answer(text='This is command "start"')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
