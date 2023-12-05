7ed5236112701918f1945e1cb618e0cbc382f217
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-05 20:21:34 +0300

    Filter with function

--- main.py
+++ main.py
@@ -12,34 +12,41 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
-class IsAdmin(BaseFilter):
-    def __init__(self, admin_ids: list[int]):
-        self.admin_ids = admin_ids
+class NumbersInMessage(BaseFilter):
+    async def __call__(self, message: Message) -> bool | dict[str, list[int]]:
+        numbers = []
+        for word in message.text.split():
+            normalized_word = word.replace('.', '').replace(',', '').strip()
+            if normalized_word.isdigit():
+                numbers.append(int(normalized_word))
 
-    async def __call__(self, message: Message) -> bool:
-        return message.from_user.id in self.admin_ids
+        if numbers:
+            return {'numbers': numbers}
+
+        return False
 
 
 @dp.message(Command(commands='start'))
 async def process_start_cmd(message: Message):
     # print(message.model_dump_json(indent=4, exclude_none=True))
     # print(message.__dict__)
-    await message.answer(text=f'This is command "start"{message.from_user.id}')
+    await message.answer(text='This is command "start"')
 
 
-@dp.message(IsAdmin(ADMIN_IDS))
-async def answer_if_admin(message: Message):
-    await message.answer(text='You are admin')
+@dp.message(F.text.lower().startswith('найди числа'),
+            NumbersInMessage())
+# Помимо объекта типа Message, принимаем в хэндлер список чисел из фильтра
+async def process_if_numbers(message: Message, numbers: list[int]):
+    await message.answer(
+            text=f'Нашел: {", ".join(str(num) for num in numbers)}')
 
 
-@dp.message()
-async def answer_if_not_admin(message: Message):
-    await message.answer(text='You are not admin')
-
-
-@dp.message(F.photo)
-async def process_send_phot(message: Message):
-    await message.answer(text='You sent photo')
+# Этот хэндлер будет срабатывать, если сообщение пользователя
+# начинается с фразы "найди числа", но в нем нет чисел
+@dp.message(F.text.lower().startswith('найди числа'))
+async def process_if_not_numbers(message: Message):
+    await message.answer(
+            text='Не нашел что-то :(')
 
 
 if __name__ == '__main__':
