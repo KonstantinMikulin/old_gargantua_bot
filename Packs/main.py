332651a67fbe2a337a86941fd0a7450be9d2dc0c
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-07 17:01:26 +0300

    Save

--- Packs/main.py
+++ Packs/main.py
@@ -1,6 +1,4 @@
-print('Это основной модуль main.py, его имя в процессе выполнения программы:', __name__)
+from pack_1.file_11 import func_1
+from pack_1.file_12 import a
 
-import pack_2
-
-print(dir())
-print(dir(pack_2))
+print(func_1(a))
--- Packs/pack_1/file_12.py
+++ Packs/pack_1/file_12.py
@@ -1,3 +1,14 @@
-print('Это файл', __name__)
+import sys
+from pprint import pprint
 
-num: int = 34
+pprint(sys.path)
+
+from pack_1.file_11 import a
+
+print(a)
+
+
+def func_2(n: int) -> int:
+    return n + n
+
+print(func_2(a))
