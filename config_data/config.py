b22c2180c14b27da4bea1cd0c4f5ffda0f0f334c
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-11 20:00:33 +0300

    Create function load_config

--- config_data/config.py
+++ config_data/config.py
@@ -22,19 +22,11 @@ class Config:
     db: DatabaseConfig
 
 
-env: Env = Env()
-env.read_env()
+def load_config(path: str | None = None) -> Config:
+    env: Env = Env()
+    env.read_env()
 
-config = Config(tg_bot=TgBot(token=env('BOT_TOKEN'), admin_ids=int(env('ADMIN_IDS'))),
-                db=DatabaseConfig(database=env('DATABASE'), db_host=env('DB_HOST'),
-                                  db_user=env('DB_USER'), db_password=env('DB_PASSWORD'))
-                )
-
-
-print('BOT_TOKEN:', config.tg_bot.token)
-print('ADMIN_IDS:', config.tg_bot.admin_ids)
-print()
-print('DATABASE:', config.db.database)
-print('DB_HOST:', config.db.db_host)
-print('DB_USER:', config.db.db_user)
-print('DB_PASSWORD:', config.db.db_password)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'), admin_ids=int(env('ADMIN_IDS'))),
+                  db=DatabaseConfig(database=env('DATABASE'), db_host=env('DB_HOST'),
+                                    db_user=env('DB_USER'), db_password=env('DB_PASSWORD'))
+                  )
