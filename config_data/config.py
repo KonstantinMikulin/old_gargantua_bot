b61008f328a353a05019d795801e29010c13feac
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-11 19:39:50 +0300

    Create config object

--- config_data/config.py
+++ config_data/config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from environs import Env
 
 
 @dataclass
@@ -19,3 +20,21 @@ class TgBot:
 class Config:
     tg_bot: TgBot
     db: DatabaseConfig
+
+
+env: Env = Env()
+env.read_env()
+
+config = Config(tg_bot=TgBot(token=env('BOT_TOKEN'), admin_ids=int(env('ADMIN_IDS'))),
+                db=DatabaseConfig(database=env('DATABASE'), db_host=env('DB_HOST'),
+                                  db_user=env('DB_USER'), db_password=env('DB_PASSWORD'))
+                )
+
+
+print('BOT_TOKEN:', config.tg_bot.token)
+print('ADMIN_IDS:', config.tg_bot.admin_ids)
+print()
+print('DATABASE:', config.db.database)
+print('DB_HOST:', config.db.db_host)
+print('DB_USER:', config.db.db_user)
+print('DB_PASSWORD:', config.db.db_password)
