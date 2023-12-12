4587942cb5163374841284186ed57596dc7e7cd0
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-12 19:56:43 +0300

    Requsests for number, location, creating poll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, KeyboardButtonPollType
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from config_data.config import Config, load_config
@@ -12,31 +12,35 @@ dp = Dispatcher()
 
 kb_builder = ReplyKeyboardBuilder()
 
-buttons: list[KeyboardButton] = [KeyboardButton(text=f'{i + 1}') for i in range(10)]
+contact_button = KeyboardButton(
+    text='Send your number',
+    request_contact=True
+)
 
-kb_builder.add(*buttons)
-kb_builder.adjust(1, 2, 4)
+geo_btn = KeyboardButton(
+    text='Send you location',
+    request_location=True
+)
 
-keyboard = kb_builder.as_markup(resize_keyboard=True)
+poll_btn = KeyboardButton(
+    text='Create poll',
+    request_poll=KeyboardButtonPollType()
+)
+
+kb_builder.row(contact_button, geo_btn, poll_btn, width=1)
+keyboard: ReplyKeyboardMarkup = kb_builder.as_markup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
 
 
 @dp.message(CommandStart())
 async def process_start_cmd(message: Message):
-    await message.answer(text='Чего кошки боятся больше?',
-                         reply_markup=keyboard
-                         )
+    await message.answer(
+        text='Here you go',
+        reply_markup=keyboard
+    )
 
-
-@dp.message(F.text == 'Собак 🦮')
-async def process_dog_answer(message: Message):
-    await message.answer(text='Да, собак'
-                         )
-
-
-@dp.message(F.text == 'Огурцов 🥒')
-async def process_dog_answer(message: Message):
-    await message.answer(text='Конечно, огурцов'
-                         )
 
 if __name__ == '__main__':
     dp.run_polling(bot)
