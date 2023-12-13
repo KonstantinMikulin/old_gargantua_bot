d55482f3018b5adfdcb091745a318f5445aa528b
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-13 22:01:44 +0300

    bot.py with logging

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
+
 from config_data.config import Config, load_config
-from handlers import other_handlers, user_handlers
+from handlers import user_handlers, other_handlers
+
+logger = logging.getLogger(__name__)
 
 
-async def main() -> None:
+async def main():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+                        '[%(asctime)s] - %(name)s - %(message)s')
+    logger.info('Starting bot')
+
     config: Config = load_config()
 
     bot = Bot(token=config.tg_bot.token)
