3ff1691f108fd646bb6ba6d8686645d3cfa4f243
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-14 19:58:16 +0300

    Save

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -9,3 +9,10 @@ router = Router()
 async def del_main_menu(message: Message, bot: Bot):
     await bot.delete_my_commands()
     await message.answer(text='Кнопка "Menu" удалена')
+
+
+# @router.message(Command(commands='delete_menu'))
+# async def process_main_menu_delete(message: Message, bot):
+#     await bot.delete_my_commands()
+#     await message.answer(text='Удалили меню. Для возвращения /restore_menu',
+#                          reply_markup=ReplyKeyboardRemove())
