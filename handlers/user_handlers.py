c3892b7d455ad3a0fcd660aedbbe8a9932ff181a
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-12-15 21:25:26 +0300

    Callback data handler

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
 
 from lexicon.lexicon import LEXICON_RU
 from keyboards.keyboards import yes_no_keyboard, url_keyboard, callback_keyboard
@@ -39,3 +39,11 @@ async def process_url_command(message: Message):
         text='Here are your big buttons',
         reply_markup=callback_keyboard
     )
+    # print(message.model_dump_json(indent=4, exclude_none=True))
+
+
+@router.callback_query(F.data.in_(['button_1 pressed', 'button_2 pressed']))
+async def process_callback(callback: CallbackQuery):
+    await callback.answer(text='We have your callback')
+    print(callback.model_dump_json(indent=4, exclude_none=True))
+
